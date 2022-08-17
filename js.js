a71bb7142063f3d97fbbab70e6a446c32f31d915
FarshadHr <main@farzadhr.ir>
2022-08-17 17:48:41 +0430

    update 2

--- js.js
+++ js.js
@@ -1,5 +1,10 @@
 var teamx;
 var finish = false;
+let checked = true;
+
+
+$(".xwin").hide();
+$(".owin").hide();
 $(document).ready(function() {
     teamx = true;
     $(".turn").text("Team X is turn");
@@ -8,23 +13,28 @@ $(document).ready(function() {
 
 $(".item").click(function() {
     if (finish == false) {
-        if ($(this).text() == "") {
+        var attr = $(this).attr("id");
+        console.log(attr);
+        if ($(".text" + attr).text() == "") {
             if (teamx == true) {
-                $(this).text("X");
+
+                $(".text" + attr).text("X");
+                $(".text" + attr).css("height", "100%");
                 teamx = false;
                 $(".turn").text("Team O is turn");
             } else {
-                $(this).text("O");
+                $(".text" + attr).text("O");
+                $(".text" + attr).css("height", "100%");
                 teamx = true;
                 $(".turn").text("Team X is turn");
             }
         }
-        check("1", "4", "7")
-        check("2", "5", "8")
-        check("3", "6", "9")
         check("1", "2", "3")
         check("4", "5", "6")
         check("7", "8", "9")
+        check("1", "4", "7")
+        check("2", "5", "8")
+        check("3", "6", "9")
         check("1", "5", "9")
         check("3", "5", "7")
         checkDraw()
@@ -40,54 +50,71 @@ function check(one, two, three) {
     var byone = false;
     var bytwo = false;
     var bythree = false;
-    if ($("#" + one).text() != "") {
-        if ($("#" + one).text() == "X") {
+    if ($(".text" + one).text() != "") {
+        if ($(".text" + one).text() == "X") {
             bxone = true;
         }
-        if ($("#" + one).text() == "O") {
+        if ($(".text" + one).text() == "O") {
             byone = true;
         }
     }
-    if ($("#" + two).text() != "") {
-        if ($("#" + two).text() == "X") {
+    if ($(".text" + two).text() != "") {
+        if ($(".text" + two).text() == "X") {
             bxtwo = true;
         }
-        if ($("#" + two).text() == "O") {
+        if ($(".text" + two).text() == "O") {
             bytwo = true;
         }
     }
-    if ($("#" + three).text() != "") {
-        if ($("#" + three).text() == "X") {
+    if ($(".text" + three).text() != "") {
+        if ($(".text" + three).text() == "X") {
             bxthree = true;
         }
-        if ($("#" + three).text() == "O") {
+        if ($(".text" + three).text() == "O") {
             bythree = true;
         }
     }
+    console.log(checked);
+    if (checked == true) {
+        if (bxone == true && bxtwo == true && bxthree == true) {
+            $(".win").text("X win");
+            $("#" + one).css("background-color", "green");
+            $("#" + two).css("background-color", "green");
+            $("#" + three).css("background-color", "green");
+            $(".turn").text("Finish");
+            setTimeout(() => {
+                $(".tictactoe").slideUp(500);
+            }, 2000);
+            setTimeout(() => {
+                $(".xwin").slideDown(500);
+            }, 2700);
+            finish = true;
+            checked = false;
+        }
+        if (byone == true && bytwo == true && bythree == true) {
+            $(".win").text("O win");
+            $("#" + one).css("background-color", "green");
+            $("#" + two).css("background-color", "green");
+            $("#" + three).css("background-color", "green");
+            $(".turn").text("Finish");
+            setTimeout(() => {
+                $(".tictactoe").slideUp(500);
+            }, 2000);
+            setTimeout(() => {
+                $(".owin").slideDown(500);
+            }, 2700);
+            finish = true;
+            checked = false;
+        }
+    }
 
-    if (bxone == true && bxtwo == true && bxthree == true) {
-        $(".win").text("X win");
-        $("#" + one).css("background-color", "green");
-        $("#" + two).css("background-color", "green");
-        $("#" + three).css("background-color", "green");
-        $(".turn").text("Finish");
-        finish = true;
-    }
-    if (byone == true && bytwo == true && bythree == true) {
-        $(".win").text("O win");
-        $("#" + one).css("background-color", "green");
-        $("#" + two).css("background-color", "green");
-        $("#" + three).css("background-color", "green");
-        $(".turn").text("Finish");
-        finish = true;
-    }
 }
 
 function checkDraw() {
     var all = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
     let draw = false;
     for (var i = 0; i < all.length; i++) {
-        if ($("#" + all[i]).text() != "") {
+        if ($(".text" + all[i]).text() != "") {
             draw = true;
         } else {
             draw = false;
